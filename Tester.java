5f781ce32d225776a813ebf71125d036575820e0
Joshin-d <102731230+Joshin-d@users.noreply.github.com>
2022-04-11 09:17:12 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,16 @@
+package com.prodapt.basics1;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LoanQueries obj=new LoanQueries();
+		obj.p=13000;
+		obj.r=12;
+		obj.t=2;
+		obj.SimpleIntrest();
+		obj.FinalAmount();
+
+	}
+
+}
